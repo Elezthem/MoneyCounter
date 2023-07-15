26112331e6125ccd5b36ed6fd15de5c36fd9b542
Elezthem <97851451+Elezthem@users.noreply.github.com>
2023-07-15 22:28:06 +0300

    Add files via upload

---
+++ CSLight/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float money;
+            int years;
+            int persent;
+
+            Console.Write("Введите количество денег, внесенных на вклад: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("На сколько лет открыт вклад? ");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Под какой процент? ");
+            persent = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < years; i++)
+            {
+                money += money / 100 * persent;
+                Console.WriteLine("В этом году у вас " + money);
+                Console.ReadKey();
+            }
+        }
+    }
+}
